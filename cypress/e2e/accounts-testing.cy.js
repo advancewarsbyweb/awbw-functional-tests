acaae1c0bd522bd79d23f46098f9a8869b81e4b0
soul4rent <kyle.a.wadsworth@gmail.com>
2025-03-22 15:44:56 -0700

    login_test adding working login test. Just have to separate locators to different fixture.

--- cypress/e2e/accounts-testing.cy.js
+++ cypress/e2e/accounts-testing.cy.js
@@ -1,16 +1,19 @@
 const { it } = require("mocha")
 
 describe('test-login', () => {
+    let data;
     before(function () {
-        cy.fixture('setup-data').then((data) => {
-          cy.visit(data.TestURL)
-        })
+        cy.fixture('setup-data').then((fdata) => {
+            data = fdata;
+            cy.visit(data.TestURL);
+        });
     })
 
-    it('Users can create accounts through the UI', () => {
-        cy.fixture('setup-data').then((data) => {
-            cy.get("#login-box-button").click()
-            cy.get('.login-input')
-        })
+    it('Users can login to accounts through the UI', function() {
+        cy.get('#login-button').click();
+        cy.get('#login-tab > .login-form > .login-input[type="text"]').type(data.StandardAccount);
+        cy.get('#login-tab > .login-form > .login-input[type="password"]').type(data.StandardAccountPass); //DO NOT STORE NON-THROWAWAY ACCOUNTS IN THE SETUP-DATA FIXTURE.
+        cy.get('#login').click();
+        cy.get('#profile-menu > [title="'+data.StandardAccount+'"]').invoke('attr', 'title').should('equal', data.StandardAccount);
     })
   })
