8320ccbfbe63b7fad011ba2ab5fc1be2f5290a1d
soul4rent <kyle.a.wadsworth@gmail.com>
2025-03-31 14:54:55 -0700

    login_test - adding example working login test (#1)
    * login_test adding working login test. Just have to separate locators to different fixture.
    
    * login_test added fixture for most login fields
    
    * login_test updating config

--- cypress/e2e/accounts-testing.cy.js
+++ cypress/e2e/accounts-testing.cy.js
@@ -1,16 +1,22 @@
 const { it } = require("mocha")
+import homepageSelectors from "../support/selectors/homepageSelectors";
 
 describe('test-login', () => {
+    let data;
     before(function () {
-        cy.fixture('setup-data').then((data) => {
-          cy.visit(data.TestURL)
-        })
+        cy.fixture('setup-data').then((fdata) => {
+            data = fdata;
+            cy.visit(data.TestURL);
+        });
     })
 
-    it('Users can create accounts through the UI', () => {
-        cy.fixture('setup-data').then((data) => {
-            cy.get("#login-box-button").click()
-            cy.get('.login-input')
-        })
+    it('Users can login to accounts through the UI', function() {
+        cy.get(homepageSelectors.loginDropdown).click();
+        cy.get(homepageSelectors.userNameField).type(data.StandardAccount);
+        cy.get(homepageSelectors.passwordField).type(data.StandardAccountPass); //DO NOT STORE NON-THROWAWAY ACCOUNT PASSWORDS IN THE SETUP-DATA FIXTURE.
+        cy.get(homepageSelectors.loginButton).click();
+        
+        //profile data that appears replacing login button
+        cy.get('#profile-menu > [title="'+data.StandardAccount+'"]').invoke('attr', 'title').should('equal', data.StandardAccount);
     })
   })
---
+++ cypress/support/selectors/homepageSelectors.js
@@ -0,0 +1,5 @@
+export default {
+    loginDropdown: '#login-button',
+    userNameField: '#login-tab > .login-form > .login-input[type="text"]',
+    passwordField: '#login-tab > .login-form > .login-input[type="password"]'
+};
