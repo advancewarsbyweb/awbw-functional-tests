1086aa536d94aae604bd6f8ffa09301eda11b55c
soul4rent <kyle.a.wadsworth@gmail.com>
2025-12-19 16:58:44 -0800

    adding basic gameplay test (build, move, capture HQ) (#12)
    * adding basic gameplay test (build, move, capture HQ)
    
    * code review update

--- cypress/e2e/basic-play-game-testing.cy.ts
+++ cypress/e2e/basic-play-game-testing.cy.ts
@@ -1,7 +1,7 @@
 import { it } from 'mocha';
 import { v4 as uuidv4 } from 'uuid';
 
-describe('create-game-tests', () => {
+describe('gameplay-tests', () => {
   let data: { TestURL: string; MapOnePrefsId: string };
   let accountData: { StandardAccount: string; StandardAccountPass: string };
   before(function () {
@@ -20,32 +20,24 @@ describe('create-game-tests', () => {
     cy.loginFrontend(accountData.StandardAccount, accountData.StandardAccountPass);
   });
 
-  // Note: If running this in production, delete the game after testing.
-  it('Basic Create Game Test', () => {
-    // create game method includes basic frontend assertions
-    cy.createGame(data.TestURL, 'Normal_Game', data.MapOnePrefsId).then((game_id: string) => {
-      // cleanup
-      cy.deleteGame(data.TestURL, game_id);
-    });
-  });
-
-  it('Basic Join and Resign Game Test', () => {
+  it('Basic Build, Move, and HQ Win Test', () => {
     const uuid = uuidv4();
+    // game creation and joining
     cy.createGame(data.TestURL, uuid, data.MapOnePrefsId);
     cy.contains('Join Again As Another Country (Hotseat)').click();
     cy.get('.submit').click();
     cy.contains('Start Game').click();
     cy.contains(uuid).should('be.visible');
-
     cy.enterGame(data.TestURL, uuid);
-    cy.resign(accountData.StandardAccountPass);
 
-    // assert initial resign message
-    cy.contains(`${accountData.StandardAccount} has resigned!`).should('be.visible');
-
-    // long timeout since it waits for the final win message
-    cy.contains(`The game is over! ${accountData.StandardAccount} is the winner!`, {
-      timeout: 10000,
-    }).should('be.visible');
+    // building and moving units
+    cy.buildUnitOnNthBuilding(0);
+    cy.endTurn();
+    cy.endTurn();
+    cy.moveNthUnitAndPerformActionOnNthBuilding(0, 1, 0) // capture HQ
+    cy.endTurn()
+    cy.endTurn()
+    cy.doubleClickNthUnitAndPerformAction(0, 0) //capture HQ
+    cy.contains(`Victory`).should('be.visible');
   });
 });
--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -14,5 +14,13 @@ declare namespace Cypress {
     enterGame(baseUrl: string, gameName: string): void;
 
     resign(password: string): void;
+
+    endTurn(): void;
+
+    buildUnitOnNthBuilding(position: number, unit?: string): void;
+
+    moveNthUnitAndPerformActionOnNthBuilding(unitPosition: number, buildingPosition: number, actionNumber: number): void
+
+    doubleClickNthUnitAndPerformAction(unitPosition: number, actionNumber: number): void
   }
 }
